ac8cd10f112694276342e8c52285b8f7dabbcd2f
Gunjit27 <98583902+Gunjit27@users.noreply.github.com>
2023-07-07 23:24:59 +0530

    Update PostData.dart

--- lib/PostData.dart
+++ lib/PostData.dart
@@ -1,208 +1,96 @@
-import 'package:api_app/DeleteData.dart';
-import 'package:api_app/PostData.dart';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'GetData.dart';
 import 'package:http/http.dart' as http;
-import 'PutData.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class PostData extends StatefulWidget {
+  const PostData({Key? key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        hintColor: Colors.amber,
-      ),
-      home: HomePage(),
-    );
-  }
+  State<PostData> createState() => _PostDataState();
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class _PostDataState extends State<PostData> {
+  final url = "https://dummy.restapiexample.com/api/v1/create";
+  String postResponse = '';
+
+  Future<void> postData() async {
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        body: {"name": "test", "salary": "123", "age": "23"},
+      );
+
+      setState(() {
+        postResponse = response.body;
+      });
+    } catch (e) {
+      print('Error occurred while sending post data: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "API App",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
+        title: const Text("POST"),
+        backgroundColor: Colors.deepPurple,
       ),
       body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color(0xFF0D47A1),
-              Color(0xFF1976D2),
-            ],
-          ),
-        ),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: AnimatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => GetData()),
-                      );
-                    },
-                    color1: Color(0xFFD69AB1),
-                    color2: Color(0xFF8E83A3),
-                    text: 'GET',
-                  ),
+        color: Colors.grey[200],
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: postData,
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                  vertical: 10.0,
                 ),
-                SizedBox(height: 20),
-                Expanded(
-                  child: AnimatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => PostData()),
-                      );
-                    },
-                    color1: Color(0xFF78A5CD),
-                    color2: Color(0xFF3C6997),
-                    text: 'POST',
-                  ),
+                textStyle: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
                 ),
-                SizedBox(height: 20),
-                Expanded(
-                  child: AnimatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => PutData()),
-                      );
-                    },
-                    color1: Color(0xFF79C7C5),
-                    color2: Color(0xFF3B8686),
-                    text: 'PUT',
-                  ),
-                ),
-                SizedBox(height: 20),
-                Expanded(
-                  child: AnimatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => DeleteData()),
-                      );
-                    },
-                    color1: Color(0xFFE4572E),
-                    color2: Color(0xFFD42A2A),
-                    text: 'DELETE',
-                  ),
-                ),
-              ],
+              ),
+              child: const Text("Send Post"),
             ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class AnimatedButton extends StatefulWidget {
-  final VoidCallback onPressed;
-  final Color color1;
-  final Color color2;
-  final String text;
-
-  const AnimatedButton({
-    required this.onPressed,
-    required this.color1,
-    required this.color2,
-    required this.text,
-  });
-
-  @override
-  _AnimatedButtonState createState() => _AnimatedButtonState();
-}
-
-class _AnimatedButtonState extends State<AnimatedButton>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<Color?> _colorAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    _animationController = AnimationController(
-      duration: Duration(milliseconds: 500),
-      vsync: this,
-    );
-
-    _colorAnimation = ColorTween(
-      begin: widget.color1,
-      end: widget.color2,
-    ).animate(_animationController);
-
-    _animationController.repeat(reverse: true);
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _colorAnimation,
-      builder: (BuildContext context, Widget? child) {
-        return Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10.0),
-            gradient: LinearGradient(
-              begin: Alignment.centerLeft,
-              end: Alignment.centerRight,
-              colors: [
-                _colorAnimation.value ?? widget.color1,
-                _colorAnimation.value ?? widget.color2,
-              ],
-            ),
-          ),
-          child: Material(
-            color: Colors.transparent,
-            child: InkWell(
-              borderRadius: BorderRadius.circular(10.0),
-              onTap: widget.onPressed,
-              child: Center(
-                child: Text(
-                  widget.text,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+            const SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: SingleChildScrollView(
+                  child: RichText(
+                    text: TextSpan(
+                      style: DefaultTextStyle.of(context).style,
+                      children: <TextSpan>[
+                        const TextSpan(
+                          text: 'Post Response:\n',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue,
+                          ),
+                        ),
+                        TextSpan(
+                          text: postResponse,
+                          style: const TextStyle(fontSize: 18),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        );
-      },
+          ],
+        ),
+      ),
     );
   }
 }
