f0f18ce4b5d84f10ff8f4052b66f472e694670dd
Gunjit27 <98583902+Gunjit27@users.noreply.github.com>
2023-07-07 23:23:29 +0530

    Update PutData.dart

--- lib/PutData.dart
+++ lib/PutData.dart
@@ -1,42 +1,94 @@
-// ignore_for_file: prefer_const_constructors, unused_import
-
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
-class PutData extends StatefulWidget {
-  const PutData({super.key});
+class PostData extends StatefulWidget {
+  const PostData({Key? key});
 
   @override
-  State<PutData> createState() => _PutDataState();
+  State<PostData> createState() => _PostDataState();
 }
 
-class _PutDataState extends State<PutData> {
-  final url = "https://dummy.restapiexample.com/api/v1/update/21/";
-  
-  void PutData() async {
-    try{
-    final response = await put(
-        Uri.parse("https://dummy.restapiexample.com/api/v1/update/21/"),
-        body: {"name": "test", "salary": "123", "age": "23"});
+class _PostDataState extends State<PostData> {
+  final url = "https://dummy.restapiexample.com/api/v1/create";
+  String postResponse = '';
 
-    print(response.body);
+  Future<void> postData() async {
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        body: {"name": "test", "salary": "123", "age": "23"},
+      );
+
+      setState(() {
+        postResponse = response.body;
+      });
+    } catch (e) {
+      print('Error occurred while sending post data: $e');
     }
-    catch(er){}
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Put"),
+        title: const Text("POST"),
+        backgroundColor: Colors.deepPurple,
       ),
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
-        child: ElevatedButton(
-          onPressed: PutData,
-          child: Text("Put Data"),
+        color: Colors.grey[200],
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: postData,
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                  vertical: 10.0,
+                ),
+                textStyle: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              child: const Text("Send Post"),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: SingleChildScrollView(
+                  child: RichText(
+                    text: TextSpan(
+                      style: DefaultTextStyle.of(context).style,
+                      children: <TextSpan>[
+                        const TextSpan(
+                          text: 'Post Response:\n',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue,
+                          ),
+                        ),
+                        TextSpan(
+                          text: postResponse,
+                          style: const TextStyle(fontSize: 18),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
