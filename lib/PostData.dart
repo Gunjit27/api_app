48837442d5e19f0fb23dfd40b039b4e39f2fe0fd
Gunjit27 <98583902+Gunjit27@users.noreply.github.com>
2023-07-07 23:22:56 +0530

    Update PostData.dart

--- lib/PostData.dart
+++ lib/PostData.dart
@@ -1,44 +1,208 @@
-// ignore_for_file: prefer_const_constructors, unused_import
-
-import 'dart:math';
-
+import 'package:api_app/DeleteData.dart';
+import 'package:api_app/PostData.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'GetData.dart';
+import 'package:http/http.dart' as http;
+import 'PutData.dart';
 
-class PostData extends StatefulWidget {
-  const PostData({super.key});
-
-  @override
-  State<PostData> createState() => _PostDataState();
+void main() {
+  runApp(MyApp());
 }
 
-class _PostDataState extends State<PostData> {
-  final url = "https://dummy.restapiexample.com/api/v1/create";
-  
-  void PostData() async {
-    try{
-    final response = await post(
-        Uri.parse("https://dummy.restapiexample.com/api/v1/create"),
-        body: {"name": "test", "salary": "123", "age": "23"});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-    print(response.body);
-    }
-    catch(er){}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        hintColor: Colors.amber,
+      ),
+      home: HomePage(),
+    );
   }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Post"),
+        title: Text(
+          "API App",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
       ),
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
-        child: ElevatedButton(
-          onPressed: PostData,
-          child: Text("Send Post"),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF0D47A1),
+              Color(0xFF1976D2),
+            ],
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: AnimatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => GetData()),
+                      );
+                    },
+                    color1: Color(0xFFD69AB1),
+                    color2: Color(0xFF8E83A3),
+                    text: 'GET',
+                  ),
+                ),
+                SizedBox(height: 20),
+                Expanded(
+                  child: AnimatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => PostData()),
+                      );
+                    },
+                    color1: Color(0xFF78A5CD),
+                    color2: Color(0xFF3C6997),
+                    text: 'POST',
+                  ),
+                ),
+                SizedBox(height: 20),
+                Expanded(
+                  child: AnimatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => PutData()),
+                      );
+                    },
+                    color1: Color(0xFF79C7C5),
+                    color2: Color(0xFF3B8686),
+                    text: 'PUT',
+                  ),
+                ),
+                SizedBox(height: 20),
+                Expanded(
+                  child: AnimatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => DeleteData()),
+                      );
+                    },
+                    color1: Color(0xFFE4572E),
+                    color2: Color(0xFFD42A2A),
+                    text: 'DELETE',
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
+    );
+  }
+}
+
+class AnimatedButton extends StatefulWidget {
+  final VoidCallback onPressed;
+  final Color color1;
+  final Color color2;
+  final String text;
+
+  const AnimatedButton({
+    required this.onPressed,
+    required this.color1,
+    required this.color2,
+    required this.text,
+  });
+
+  @override
+  _AnimatedButtonState createState() => _AnimatedButtonState();
+}
+
+class _AnimatedButtonState extends State<AnimatedButton>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Color?> _colorAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: Duration(milliseconds: 500),
+      vsync: this,
+    );
+
+    _colorAnimation = ColorTween(
+      begin: widget.color1,
+      end: widget.color2,
+    ).animate(_animationController);
+
+    _animationController.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _colorAnimation,
+      builder: (BuildContext context, Widget? child) {
+        return Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [
+                _colorAnimation.value ?? widget.color1,
+                _colorAnimation.value ?? widget.color2,
+              ],
+            ),
+          ),
+          child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              borderRadius: BorderRadius.circular(10.0),
+              onTap: widget.onPressed,
+              child: Center(
+                child: Text(
+                  widget.text,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
